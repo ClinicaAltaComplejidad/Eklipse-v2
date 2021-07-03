38eda1f3c6a1aa6d477f8ee96641aa9ca2c02259
Edwin Molina <delgadoedwin199@gmail.com>
2021-07-03 09:34:11 -0500

    [dev] Cambiando POST por GET en router invoives

--- api/billing/routes.js
+++ api/billing/routes.js
@@ -5,7 +5,7 @@ const {uploadFile, getInvoices} = require('./invoices/controller');
 //scripts
 const tokenValidate = require('../../scripts/token');
 
-routeInvoices.post('/invoices', tokenValidate.verifyToken, getInvoices);
+routeInvoices.get('/invoices', tokenValidate.verifyToken, getInvoices);
 routeInvoices.post('/invoices/upload', tokenValidate.verifyToken, uploadFile);
 
 module.exports = {
